9cf70493081892e48dcf6cea7c5fe986dd5a7443
Anatolii <dinexpod@gmail.com>
2019-03-12 15:41:13 +0200

    - refactored

--- src/mate/academy/hw/zoo/aviaries/BirdCage.java
+++ src/mate/academy/hw/zoo/aviaries/BirdCage.java
@@ -1,6 +1,7 @@
 package mate.academy.hw.zoo.aviaries;
 
 import mate.academy.hw.zoo.animals.Animal;
+import mate.academy.hw.zoo.animals.Birds;
 
-public class BirdCage extends Cage implements Zoo<Animal> {
+public class BirdCage extends Cage<Birds> {
 }
